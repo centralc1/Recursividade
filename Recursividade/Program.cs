81e982e8780d53b9ad2e2fa81153749fe018f437
João Vítor <Joo Vtor@DESKTOP-1O6R9P5>
2023-08-18 00:10:35 -0300

    Adicionar arquivos de projeto.

---
+++ Recursividade/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursividade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Recursivo r = new Recursivo();
+            r.ExecutarRecursivo("Olá Mundo", 5);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Recursividade/Recursivo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursividade
+{
+    class Recursivo
+    {
+        // Não Recursivo 
+        public void Executar (string mensagem, int n) 
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(mensagem);
+            }
+        }
+
+        // É Recursivo
+        public void ExecutarRecursivo(string mensagem, int n)
+        {
+            if (n > 0)
+            {
+                Console.WriteLine(mensagem);
+                ExecutarRecursivo(mensagem, n - 1);
+            }
+        }
+    }
+}
